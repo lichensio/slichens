d2fc357c6709b84d0965ebb2df0cfa1143caf8aa
emoolva <olivier.vandermoten@lichens.io>
2023-04-17 23:55:40 +0200

    root correted

--- cmd/root.go
+++ cmd/root.go
@@ -16,16 +16,15 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "slichens",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "slichens, CLI tool to analyse siretta survey csv files",
+	Long: `slichens a CLI tool to analyse siretta survey csv file. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Calculate average, min, max and sd of a survey per MNO and per Band.
+Compare outdoor to indoor survey to evaluate indoor attenuation.
+Compare two indoor surveys in order to evaluate possible improvements.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) { fmt.Println("Hello CLI") },
+	Run: func(cmd *cobra.Command, args []string) { fmt.Println("Hello slichens tool") },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
